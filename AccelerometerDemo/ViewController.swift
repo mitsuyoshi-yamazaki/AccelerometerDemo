44b1ae16ef58131cedfe5464ddee4245a9b46c76
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-03 15:10:48 +0900

    show acceleration on the device and console

--- AccelerometerDemo/ViewController.swift
+++ AccelerometerDemo/ViewController.swift
@@ -12,6 +12,13 @@ import CoreMotion
 // https://www.hackingwithswift.com/example-code/system/how-to-use-core-motion-to-read-accelerometer-data
 final class ViewController: UIViewController {
 
+  @IBOutlet fileprivate weak var accelerationLabel: UILabel! {
+    didSet {
+      let fontSize = accelerationLabel.font.pointSize
+      accelerationLabel.font = UIFont.monospacedDigitSystemFont(ofSize: fontSize, weight: UIFontWeightMedium)
+    }
+  }
+  
   fileprivate let motionManager = CMMotionManager()
   
   override func viewDidLoad() {
@@ -34,7 +41,17 @@ final class ViewController: UIViewController {
         return
       }
       
-      print(data.acceleration)
+      let format = "%1.3f"
+      let xString = data.acceleration.x.convertToStringWithSign(format: format)
+      let yString = data.acceleration.y.convertToStringWithSign(format: format)
+      let zString = data.acceleration.z.convertToStringWithSign(format: format)
+      let formattedResult = "(x: \(xString), y : \(yString), z : \(zString))"
+      
+      print(formattedResult)
+      
+      DispatchQueue.main.async {
+        _self.accelerationLabel.text = formattedResult
+      }
     }
   }
   
@@ -42,5 +59,13 @@ final class ViewController: UIViewController {
     super.viewWillDisappear(animated)
     
     // TODO: tear down
+  }
+}
+
+extension Double {
+  func convertToStringWithSign(format: String) -> String {
+    
+    let rawString = String(format: format, self)
+    return self < 0 ? rawString : "+" + rawString
   }
 }
