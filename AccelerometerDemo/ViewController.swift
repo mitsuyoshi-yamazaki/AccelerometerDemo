446c7b444e5b4aa368a49c0cf2c6ba05bcf44e99
Mitsuyoshi Yamazaki <mitsuyoshi.yamazaki@speee.jp>
2017-03-03 14:41:43 +0900

    can read accelerometer

--- AccelerometerDemo/ViewController.swift
+++ AccelerometerDemo/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import CoreMotion
 
-class ViewController: UIViewController {
+// https://www.hackingwithswift.com/example-code/system/how-to-use-core-motion-to-read-accelerometer-data
+final class ViewController: UIViewController {
 
+  fileprivate let motionManager = CMMotionManager()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    motionManager.startAccelerometerUpdates(to: OperationQueue.main) { [weak self] (dataOptional, error) in
+      guard
+        let _self = self,
+        let data = dataOptional,
+        error == nil
+      else {
+        print("accelerometerUpdates failed")
+        print(dataOptional ?? "data is nil")
+        print(error ?? "error is nil ")
+        return
+      }
+      
+      print(data.acceleration)
+    }
   }
-
-
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    
+    // TODO: tear down
+  }
 }
-
